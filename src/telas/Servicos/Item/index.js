e7ba0a9d9c8b5560fd4ba78e1e7489c639c39299
Lucas Henrique Gomes <lucas.henrique.s.go@gmail.com>
2022-02-28 15:01:37 -0300

    Including List

--- src/telas/Servicos/Item/index.js
+++ src/telas/Servicos/Item/index.js
@@ -1,11 +1,35 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, Button } from "react-native";
 
-export default function Item({nome, preco, descricao}){
-    return <>
-    <Text>{nome}</Text>
-    <Text>{preco}</Text>
-    <Text>{descricao}</Text>
-    </>
+
+import estilos from "./estilos";
+import CampoInteiro from "../../../componentes/CampoInteiro";
+
+export default function Item({ nome, preco, descricao }) {
+
+    const [quantidade, setQuantidade] = useState(0);
+
+    return (
+        <>
+            <View style={estilos.informacao}>
+                <Text style={estilos.nome}>{nome}</Text>
+                <Text style={estilos.descricao}>{descricao}</Text>
+                <Text style={estilos.preco}>{preco}</Text>
+            </View>
+            <View style={estilos.carrinho}>
+                <View style={estilos.valor}>
+                    <Text style={estilos.descricao}>Quantidade:</Text>
+                    <CampoInteiro valor={quantidade} acao={setQuantidade}></CampoInteiro>
+                </View>
+                <View style={estilos.valor}>
+                    <Text style={estilos.descricao}>Preço:</Text>
+                    <Text style={estilos.preco}>0</Text>
+
+                </View>
+                <Button title="Adicionar"></Button>
+
+            </View>
+            <View style={estilos.divisor} />
+        </>)
 
 }
--- src/telas/Servicos/index.js
+++ src/telas/Servicos/index.js
@@ -31,8 +31,9 @@ export default function Servicos(){
             <StatusBar/>
             <Text>Serviços</Text>
             <FlatList data={servicos}
+             removeClippedSubviews={false}
              renderItem={({item}) =>  <Item {...item}/>} 
-             keyExtractor={(id) => String(id)}>
+             keyExtractor={({id}) => String(id)}>
 
             </FlatList>
         </SafeAreaView>
